9f2cc23111c9e146e863d905c11aa833d054bdc4
Galahorn <aleks_dragonil@mail.ru>
2022-04-11 12:09:57 +0500

    Добавил шаблон функции swap

---
+++ Home work C++.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+template<typename T>
+void swap(T numbers[], int i, int j) {
+    T temp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = temp;
+}
+
+int main()
+{
+    
+}
+
