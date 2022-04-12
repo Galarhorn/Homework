fa06264469cc6724a5deac3a2a589acadebd5b20
Galahorn <aleks_dragonil@mail.ru>
2022-04-12 16:21:45 +0500

    Добавил шаблон печати одномерного массива

--- Home work C++.cpp
+++ Home work C++.cpp
@@ -1,10 +1,19 @@
 ﻿#include <iostream>
+using namespace std;
 
 template<typename T>
 void swap(T numbers[], int i, int j) {
     T temp = numbers[i];
     numbers[i] = numbers[j];
     numbers[j] = temp;
+}
+
+template<typename T, int size>
+void print(T(&arr)[size])
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << ' ';
+    cout << '\n';
 }
 
 int main()
